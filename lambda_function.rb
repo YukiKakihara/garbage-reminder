aedcacfe26846c47641546958523e4cd160a2bfb
yuki.kakihara <argentina.kyg@gmail.com>
2022-05-21 10:45:46 +0900

    ボットのメッセージを書いた

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,5 +1,20 @@
-require 'json'
+require 'date'
 
 def lambda_handler(event:, context:)
-    { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
+  day = Date.today.wday
+  msg = message(day)
+  { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
+end
+
+def message(day)
+  case day
+  when 1, 5
+    '今日は「燃えるゴミ」の収集日です'
+  when 2
+    '今日は「缶・ビン・ペットボトル」の収集日です'
+  when 6
+    '今日は「プラスチックごみ」の収集日です'
+  else
+    '今日はゴミの収集日ではありません'
+  end
 end
