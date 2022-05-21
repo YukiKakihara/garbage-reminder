8dbb9d4ed6211c1fa3a54793a61ab6d76ce62502
yuki.kakihara <argentina.kyg@gmail.com>
2022-05-21 10:57:24 +0900

    引数名の修正

--- lambda_function.rb
+++ lambda_function.rb
@@ -2,7 +2,7 @@ require 'date'
 require 'net/http'
 require 'uri'
 
-def lambda_handler(_event:, _context:)
+def lambda_handler(event:, context:)
   day = Date.today.wday
   msg = message(day)
   res = post_message_to_line(msg)
